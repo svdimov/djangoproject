356ce0c002122941a749c9a43f4f043b8970e92e
stefoto85 <you@example.com>
2024-09-03 11:28:51 +0300

    first commit

---
+++ djangoproject/phonebook/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from djangoproject.phonebook.models import Contact
+
+
+def landing_page(request):
+    contacts = Contact.objects.all()
+    context = {
+        'contacts': contacts
+    }
+
+    return render(request, 'phonebook/index.html', context)
+
+
+def create_contact(request):
+    name = request.POST['name']
+    number = request.POST['number']
+    contact = Contact(
+        name=name,
+        number=number
+    )
+    contact.save()
+    return redirect('landing-page')
